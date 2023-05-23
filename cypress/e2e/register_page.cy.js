849a03264c3e185bd025e9e88e1bfe763b2a32a6
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-05-23 21:43:54 +0700

    Fix code 3

---
+++ cypress/e2e/register_page.cy.js
@@ -0,0 +1,15 @@
+describe('The Register Page UI', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('host') + '/login')
+      cy.get('[name^=email]').type('admin@gmail.com')
+      cy.get('[name^=password]').type('Admin123')
+      cy.get('[class^=LoginPage_Button]').contains('Log In').click()
+      cy.wait(3000).visit(Cypress.env('host') + '/register')
+    })
+    it('header', () => {
+      cy.get('[class^=Header_Title]').contains('Đăng kí');
+    })
+    // it('whiteboard title', () => {
+    //   cy.get('[class^=Whiteboard_Title]').contains('Đăng kí');
+    // })
+  })
